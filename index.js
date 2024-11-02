9029606e3414547803ede1b2b8c76f6a0d3190f5
cmyk52 <j.lmarambio@hotmail.com>
2024-11-01 21:48:01 -0300

    update routes

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 const express = require("express");
 const app = express();
+const movies = require("./movies.json");
 const PORT = process.env.PORT || 3000;
 
+//middlewares
+
+//routes
+
+//get.routes
 app.get("/", (req, res) => {
     res.send("hola mundo");
 })
 
+app.get("/movies", (req, res) => {
+    res.send(movies);
+
+
+})
+
+//put.routes
+
+
+//patch.routes
+
+
+//delete.routes
 
 
 
+
+//404.routes
+
+app.use((req, res) => {
+    res.status(404).send("Not found");
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
